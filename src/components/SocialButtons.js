be3dd0f9bf637fb882538a3c8cd19e707b551fc1
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-05-25 20:57:22 -0600

    Add resume download

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -3,6 +3,7 @@ import {
     faLinkedin,
     faGithub
   } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 const SocialButtons = () => {
     return ( 
@@ -10,8 +11,11 @@ const SocialButtons = () => {
           <a href='https://linkedin.com/in/evansmith04/' target='_blank' rel='noreferrer'>
             <FontAwesomeIcon icon={faLinkedin} size='2x' />
           </a>
-          <a href='https://github.com/EvanSmith93' target='_blank' rel='noreferrer' className='last-social-button'>
+          <a href='https://github.com/EvanSmith93' target='_blank' rel='noreferrer'>
             <FontAwesomeIcon icon={faGithub} size='2x' />
+          </a>
+          <a href='EvanSmithResume.docx' target='_blank' rel='noreferrer' className='last-social-button'>
+            <FontAwesomeIcon icon={faFile} size='2x' />
           </a>
         </div>
      );
