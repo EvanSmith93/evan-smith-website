434f4187a9c681e222ea6bf0ee4fe04fe265e994
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-05-25 21:08:29 -0600

    Add vercel analytics and clear some dependency issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import Navbar from './Navbar';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <div className="App">
         <div className="content">
           <Routes>
