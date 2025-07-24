a39d01551acc48c634c2a2a411a372ed3b2da443
EvanSmith93 <evan.smith993@gmail.com>
2025-07-23 18:55:35 -0600

    small updates to site content

--- src/sections/Education.js
+++ src/sections/Education.js
@@ -1,69 +1,85 @@
-import data from '../data.json'
+import data from "../data.json";
 
 const Education = () => {
-    return ( 
-        <section className='row darker-bg p-5' id='education'>
-            <h1 className='mb-4'>Education</h1>
-            <div className='col-sm-6 mb-3 school-card'>
-                <div className='flex-column'>
-                    { /* Basic Info */ }
-                    <div className='card p-3 mb-4'>
-                        <div className='corner-image-card-header'>
-                            <h6 className='gray'><i>{data.education.start} - {data.education.graduation}</i></h6>
-                            <h3 className='mb-2'>{data.education.school.name}</h3>
-                        </div>
-                        <h4 className='highlight'>{data.education.degree} in {data.education.major}</h4>
-                        <ul>
-                            <li>{data.education.gpa} / 4.00 GPA</li>
-                            <li>Dean's List {data.education.deansList} (top {data.education.deansListPercentile}% of college)</li>
-                            <li>{data.education.sat} / 1600 SAT (top {data.education.satPercentile}% of nation)</li>
-                            <li>Minors in {data.education.minors}</li>
-                            <li>Emphasis in {data.education.emphasis}</li>
-                        </ul>
-                    </div>
-                    <a href={data.education.school.link} target='_blank' rel="noreferrer">
-                        <img src={data.education.school.image} alt={data.education.school.name + " Logo"} className="corner-image"></img>
-                    </a>
+  return (
+    <section className="row darker-bg p-5" id="education">
+      <h1 className="mb-4">Education</h1>
+      <div className="col-sm-6 mb-3 school-card">
+        <div className="flex-column">
+          {/* Basic Info */}
+          <div className="card p-3 mb-4">
+            <div className="corner-image-card-header">
+              <h6 className="gray">
+                <i>
+                  {data.education.start} - {data.education.graduation}
+                </i>
+              </h6>
+              <h3 className="mb-2">{data.education.school.name}</h3>
+            </div>
+            <h4 className="highlight">
+              {data.education.degree} in {data.education.major}
+            </h4>
+            <ul>
+              <li>{data.education.gpa} / 4.00 GPA</li>
+              <li>
+                Dean's List {data.education.deansList} (top{" "}
+                {data.education.deansListPercentile}% of college)
+              </li>
+              <li>
+                {data.education.sat} / 1600 SAT (top{" "}
+                {data.education.satPercentile}% of nation)
+              </li>
+              <li>Minor in {data.education.minor}</li>
+            </ul>
+          </div>
+          <a href={data.education.school.link} target="_blank" rel="noreferrer">
+            <img
+              src={data.education.school.image}
+              alt={data.education.school.name + " Logo"}
+              className="corner-image"
+            ></img>
+          </a>
 
-                    { /* Activities */ }
-                    <div className='card p-3'>
-                        <h2 className='mb-3'>Activities</h2>
-                        <div className='activities row'>
-                            {data.education.activities.map((activity, index) => {
-                                return (
-                                    <div className='col-sm-4'>
-                                        <a href={activity.link} target='_blank' rel="noreferrer">
-                                            <img src={activity.image} alt={activity.name}></img>
-                                            <p className='gray'>{activity.name}</p>
-                                        </a>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
+          {/* Activities */}
+          <div className="card p-3">
+            <h2 className="mb-3">Activities</h2>
+            <div className="activities row">
+              {data.education.activities.map((activity, index) => {
+                return (
+                  <div className="col-sm-4">
+                    <a href={activity.link} target="_blank" rel="noreferrer">
+                      <img src={activity.image} alt={activity.name}></img>
+                      <p className="gray">{activity.name}</p>
+                    </a>
+                  </div>
+                );
+              })}
             </div>
-            
-            { /* Relevant Coursework */ }
-            <div className='col-sm-6'>
-                <div className='card p-3'>
-                    <h2>Relevant Coursework</h2>
-                    <div className='row mb-3'>
-                        {data.education.courses.map((course, index) => {
-                            return (
-                                <div className='col-sm-6'>
-                                    <p>
-                                        <i className='gray'>{course.number}</i>
-                                        <br/>{course.name}
-                                    </p>
-                                </div>
-                            )
-                        })}
-                    </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Relevant Coursework */}
+      <div className="col-sm-6">
+        <div className="card p-3">
+          <h2>Relevant Coursework</h2>
+          <div className="row mb-3">
+            {data.education.courses.map((course, index) => {
+              return (
+                <div className="col-sm-6">
+                  <p>
+                    <i className="gray">{course.number}</i>
+                    <br />
+                    {course.name}
+                  </p>
                 </div>
-            </div>
-        </section>
-     );
-}
- 
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Education;
